b97ced91069606de3a76aaaaf1d9aec12db9a46a
Mario Perić <pericmario96@gmail.com>
2017-01-07 03:28:28 +0100

    lab6

---
+++ lab6/lab6.vhd
@@ -0,0 +1,124 @@
+----------------------------------------------------------------------------------
+-- Company:        FTN
+-- Engineer:       Mario Perić
+-- 
+-- Create Date:    12:25:47 12/13/2016 
+-- Design Name: 
+-- Module Name:    lab6 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity zadata6 is
+    Port ( iCLK : in  STD_LOGIC;
+           inRST : in  STD_LOGIC;
+           oRED : out  STD_LOGIC;
+           oYELLOW : out  STD_LOGIC;
+           oGREEN : out  STD_LOGIC);
+end zadata6;
+
+architecture Behavioral of zadata6 is
+	
+	type tSTATE is (IDLE, RED, RED_YELLOW, YELLOW, GREEN);
+	signal sSTATE : tSTATE;
+	signal sRL_CNT_TC : STD_LOGIC;
+	signal sGL_CNT_TC : STD_LOGIC;
+	signal sRL_CNT : STD_LOGIC_VECTOR(2 downto 0);
+	signal sGL_CNT : STD_LOGIC_VECTOR(3 downto 0);
+	signal sRL_CNT_EN : STD_LOGIC;
+	signal sGL_CNT_EN : STD_LOGIC;
+
+begin
+	
+	process (iCLK, inRST) begin
+		if (inRST = '0') then
+			sSTATE <= IDLE;
+		elsif (rising_edge(iCLK)) then
+			case sSTATE is
+				when IDLE => 
+					sSTATE <= RED;
+					
+				when RED =>
+					if (sRL_CNT_TC = '1') then
+						sSTATE <= RED_YELLOW;
+					elsif (sRL_CNT_TC = '0') then
+						sSTATE <= RED;
+					end if;
+					
+				when RED_YELLOW => 
+					sSTATE <= GREEN;
+					
+				when GREEN =>
+					if (sGL_CNT_TC = '1') then
+						sSTATE <= YELLOW;
+					elsif (sGL_CNT_TC = '0') then
+						sSTATE <= GREEN;
+					end if;
+					
+				when YELLOW =>
+					sSTATE <= RED;
+				end case;
+		end if;
+	end process;
+	
+	process (iCLK, inRST) begin
+		if (inRST = '0') then
+			sRL_CNT <= (others => '0');
+		elsif (rising_edge(iCLK)) then
+			if (sRL_CNT_EN = '1') then
+				if (sRL_CNT = 5) then
+					sRL_CNT <= (others => '0');
+				else
+					sRL_CNT <= sRL_CNT + "001";
+				end if;
+			end if;
+		end if;
+	end process;
+	
+	process (iCLK, inRST) begin
+		if (inRST = '0') then
+			sGL_CNT <= (others => '0');
+		elsif (rising_edge(iCLK)) then
+			if (sGL_CNT_EN = '1') then
+				if (sGL_CNT = 9) then
+					sGL_CNT <= (others => '0');
+				else
+					sGL_CNT <= sGL_CNT + "0001";
+				end if;
+			end if;
+		end if;
+	end process;
+	
+	sRL_CNT_EN <= '1' when (sSTATE = RED) else '0';
+	sGL_CNT_EN <= '1' when (sSTATE = GREEN) else '0';
+	
+	sRL_CNT_TC <= '1' when sRL_CNT = 5 else '0';
+	sGL_CNT_TC <= '1' when sGL_CNT = 9 else '0';
+	
+	oRED <= '1' when (sSTATE = RED or sSTATE = RED_YELLOW) else '0';
+	oYELLOW <= '1' when (sSTATE = RED_YELLOW or sSTATE = YELLOW) else '0';
+	oGREEN <= '1' when (sSTATE = GREEN) else '0';
+
+end Behavioral;
+
---
+++ lab6/lab6_tb.vhd
@@ -0,0 +1,97 @@
+--------------------------------------------------------------------------------
+-- Company:       FTN
+-- Engineer:      Mario Perić
+--
+-- Create Date:   13:01:08 12/13/2016
+-- Design Name:   
+-- Module Name:   C:/Users/student/Desktop/RA14-2015/zadata6/zadata6_tb.vhd
+-- Project Name:  zadata6
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: zadata6
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY zadata6_tb IS
+END zadata6_tb;
+ 
+ARCHITECTURE behavior OF zadata6_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT zadata6
+    PORT(
+         iCLK : IN  std_logic;
+         inRST : IN  std_logic;
+         oRED : OUT  std_logic;
+         oYELLOW : OUT  std_logic;
+         oGREEN : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal iCLK : std_logic := '0';
+   signal inRST : std_logic := '0';
+
+ 	--Outputs
+   signal oRED : std_logic;
+   signal oYELLOW : std_logic;
+   signal oGREEN : std_logic;
+
+   -- Clock period definitions
+   constant iCLK_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: zadata6 PORT MAP (
+          iCLK => iCLK,
+          inRST => inRST,
+          oRED => oRED,
+          oYELLOW => oYELLOW,
+          oGREEN => oGREEN
+        );
+
+   -- Clock process definitions
+   iCLK_process :process
+   begin
+		iCLK <= '0';
+		wait for iCLK_period/2;
+		iCLK <= '1';
+		wait for iCLK_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+		
+		inRST <= '0';
+		wait for 100ns;
+		inRST <= '1';
+		
+      wait;
+   end process;
+
+END;
